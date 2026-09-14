04e59a0fbd0f9ddb57ba7938c0374037fab4b426
Miguel Barrera <miguelbarrera.wd@gmail.com>
2026-09-14 11:18:00 -0500

    feat(javascript-data-transformer): add barrel export (FR-10/FR-11)

---
+++ javascript-data-transformer/src/index.js
@@ -0,0 +1,5 @@
+export { filterItems } from './filterItems.js';
+export { searchItems } from './searchItems.js';
+export { sortItems } from './sortItems.js';
+export { groupItems } from './groupItems.js';
+export { paginateItems } from './paginateItems.js';
